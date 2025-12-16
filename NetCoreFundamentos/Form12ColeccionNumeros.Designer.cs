4983080f847f9c042e28356192edd0ce998b7708
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-12-16 12:38:49 +0100

    Form01ColeccionNumeros

---
+++ NetCoreFundamentos/Form12ColeccionNumeros.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form12ColeccionNumeros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lstNumeros = new ListBox();
+            btnGenerar = new Button();
+            btnMostrarDatos = new Button();
+            label2 = new Label();
+            txtSumaTotal = new TextBox();
+            label3 = new Label();
+            txtPares = new TextBox();
+            label4 = new Label();
+            txtImpares = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(63, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Numeros";
+            // 
+            // lstNumeros
+            // 
+            lstNumeros.FormattingEnabled = true;
+            lstNumeros.Location = new Point(67, 63);
+            lstNumeros.Name = "lstNumeros";
+            lstNumeros.Size = new Size(171, 229);
+            lstNumeros.TabIndex = 1;
+            // 
+            // btnGenerar
+            // 
+            btnGenerar.Location = new Point(354, 63);
+            btnGenerar.Name = "btnGenerar";
+            btnGenerar.Size = new Size(120, 23);
+            btnGenerar.TabIndex = 2;
+            btnGenerar.Text = "Generar numeros";
+            btnGenerar.UseVisualStyleBackColor = true;
+            btnGenerar.Click += btnGenerar_Click;
+            // 
+            // btnMostrarDatos
+            // 
+            btnMostrarDatos.Location = new Point(354, 108);
+            btnMostrarDatos.Name = "btnMostrarDatos";
+            btnMostrarDatos.Size = new Size(120, 34);
+            btnMostrarDatos.TabIndex = 3;
+            btnMostrarDatos.Text = "Mostrar datos";
+            btnMostrarDatos.UseVisualStyleBackColor = true;
+            btnMostrarDatos.Click += btnMostrarDatos_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(333, 188);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Suma:";
+            // 
+            // txtSumaTotal
+            // 
+            txtSumaTotal.Location = new Point(399, 180);
+            txtSumaTotal.Name = "txtSumaTotal";
+            txtSumaTotal.Size = new Size(100, 23);
+            txtSumaTotal.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(334, 224);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Pares:";
+            // 
+            // txtPares
+            // 
+            txtPares.Location = new Point(401, 223);
+            txtPares.Name = "txtPares";
+            txtPares.Size = new Size(100, 23);
+            txtPares.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(333, 267);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Impares:";
+            // 
+            // txtImpares
+            // 
+            txtImpares.Location = new Point(405, 267);
+            txtImpares.Name = "txtImpares";
+            txtImpares.Size = new Size(100, 23);
+            txtImpares.TabIndex = 9;
+            // 
+            // Form12ColeccionNumeros
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtImpares);
+            Controls.Add(label4);
+            Controls.Add(txtPares);
+            Controls.Add(label3);
+            Controls.Add(txtSumaTotal);
+            Controls.Add(label2);
+            Controls.Add(btnMostrarDatos);
+            Controls.Add(btnGenerar);
+            Controls.Add(lstNumeros);
+            Controls.Add(label1);
+            Name = "Form12ColeccionNumeros";
+            Text = "Form12ColeccionNumeros";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox lstNumeros;
+        private Button btnGenerar;
+        private Button btnMostrarDatos;
+        private Label label2;
+        private TextBox txtSumaTotal;
+        private Label label3;
+        private TextBox txtPares;
+        private Label label4;
+        private TextBox txtImpares;
+    }
+}
---
+++ NetCoreFundamentos/Form12ColeccionNumeros.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form12ColeccionNumeros : Form
+    {
+        public Form12ColeccionNumeros()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            this.lstNumeros.Items.Clear();
+            for (int i = 1; i <= 10; i++)
+            {
+                int numero = new Random().Next(1, 101);
+                this.lstNumeros.Items.Add(numero);
+            }
+        }
+
+        private void btnMostrarDatos_Click(object sender, EventArgs e)
+        {
+
+
+            int sumaTotal = 0;
+            int sumaPares = 0;
+            int sumaImpares = 0;
+
+            foreach(int numero in this.lstNumeros.Items)
+            {
+                sumaTotal += numero;
+
+                if(numero % 2 == 0)
+                {
+                    sumaPares += numero;
+                }
+                else
+                {
+                    sumaImpares += numero;
+                }
+
+            }
+            this.txtSumaTotal.Text = sumaTotal.ToString();
+            this.txtPares.Text = sumaPares.ToString();
+            this.txtImpares.Text = sumaImpares.ToString();
+        }
+    }
+}
