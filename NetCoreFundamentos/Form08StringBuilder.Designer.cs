7c8d75b463febc696f5849aa5e9f6eaeb23d39e7
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-12-16 09:48:59 +0100

    Form08StringBuilder

---
+++ NetCoreFundamentos/Form08StringBuilder.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form08StringBuilder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtTexto = new RichTextBox();
+            lblTiempo = new Label();
+            btnInvertirString = new Button();
+            btnInvertirStringBuilder = new Button();
+            lblInvertirString = new Label();
+            lblStringBuilder = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(59, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(140, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Copie texto para trabajar:";
+            // 
+            // txtTexto
+            // 
+            txtTexto.Location = new Point(61, 64);
+            txtTexto.Name = "txtTexto";
+            txtTexto.Size = new Size(615, 303);
+            txtTexto.TabIndex = 1;
+            txtTexto.Text = "";
+            // 
+            // lblTiempo
+            // 
+            lblTiempo.AutoSize = true;
+            lblTiempo.BackColor = Color.Transparent;
+            lblTiempo.Location = new Point(68, 396);
+            lblTiempo.Name = "lblTiempo";
+            lblTiempo.Size = new Size(0, 15);
+            lblTiempo.TabIndex = 2;
+            // 
+            // btnInvertirString
+            // 
+            btnInvertirString.Location = new Point(59, 444);
+            btnInvertirString.Name = "btnInvertirString";
+            btnInvertirString.Size = new Size(280, 52);
+            btnInvertirString.TabIndex = 3;
+            btnInvertirString.Text = "Invertir string";
+            btnInvertirString.UseVisualStyleBackColor = true;
+            btnInvertirString.Click += btnInvertirString_Click;
+            // 
+            // btnInvertirStringBuilder
+            // 
+            btnInvertirStringBuilder.Location = new Point(387, 444);
+            btnInvertirStringBuilder.Name = "btnInvertirStringBuilder";
+            btnInvertirStringBuilder.Size = new Size(289, 52);
+            btnInvertirStringBuilder.TabIndex = 4;
+            btnInvertirStringBuilder.Text = "Invertir StringBuilder";
+            btnInvertirStringBuilder.UseVisualStyleBackColor = true;
+            btnInvertirStringBuilder.Click += btnInvertirStringBuilder_Click;
+            // 
+            // lblInvertirString
+            // 
+            lblInvertirString.AutoSize = true;
+            lblInvertirString.Location = new Point(127, 526);
+            lblInvertirString.Name = "lblInvertirString";
+            lblInvertirString.Size = new Size(0, 15);
+            lblInvertirString.TabIndex = 5;
+            // 
+            // lblStringBuilder
+            // 
+            lblStringBuilder.AutoSize = true;
+            lblStringBuilder.Location = new Point(425, 523);
+            lblStringBuilder.Name = "lblStringBuilder";
+            lblStringBuilder.Size = new Size(0, 15);
+            lblStringBuilder.TabIndex = 6;
+            // 
+            // Form08StringBuilder
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 567);
+            Controls.Add(lblStringBuilder);
+            Controls.Add(lblInvertirString);
+            Controls.Add(btnInvertirStringBuilder);
+            Controls.Add(btnInvertirString);
+            Controls.Add(lblTiempo);
+            Controls.Add(txtTexto);
+            Controls.Add(label1);
+            Name = "Form08StringBuilder";
+            Text = "Form08StringBuilder";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private RichTextBox txtTexto;
+        private Label lblTiempo;
+        private Button btnInvertirString;
+        private Button btnInvertirStringBuilder;
+        private Label lblInvertirString;
+        private Label lblStringBuilder;
+    }
+}
---
+++ NetCoreFundamentos/Form08StringBuilder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form08StringBuilder : Form
+    {
+        public Form08StringBuilder()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInvertirString_Click(object sender, EventArgs e)
+        {
+            string cadena = this.txtTexto.Text;
+            string cadenaInvertida = "";
+
+            Stopwatch crono = new Stopwatch();
+
+            crono.Start();
+
+            for (int i = cadena.Length - 1; i >= 0; i--)
+            {
+                cadenaInvertida += cadena[i];
+            }
+            crono.Stop();
+            this.lblTiempo.Text = "Segundos: " + crono.Elapsed.Seconds + ", Milisegundos: " + crono.Elapsed.Microseconds;
+            this.lblInvertirString.Text = cadenaInvertida;
+        }
+
+        private void btnInvertirStringBuilder_Click(object sender, EventArgs e)
+        {
+            StringBuilder cadena = new StringBuilder();
+            cadena.Append(this.txtTexto.Text);
+            string cadenaInvertida = "";
+
+            Stopwatch crono = new Stopwatch();
+
+            crono.Start();
+
+            for (int i = cadena.Length - 1; i >= 0; i--)
+            {
+                cadenaInvertida += cadena[i];
+            }
+            crono.Stop();
+            this.lblTiempo.Text = "Segundos: " + crono.Elapsed.Seconds + ", Milisegundos: " + crono.Elapsed.Microseconds;
+            this.lblStringBuilder.Text = cadenaInvertida;
+        }
+    }
+}
