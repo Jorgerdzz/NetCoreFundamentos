3e685471b5e16e787ee44872ea757ca7d3a7d697
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-12-17 10:26:41 +0100

    Form14Metodos

---
+++ NetCoreFundamentos/Form14Metodos.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form14Metodos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNumero = new Label();
+            txtNumero = new TextBox();
+            lblResultado = new Label();
+            btnDobleValor = new Button();
+            btnDobleReferencia = new Button();
+            btnObjetoReferencia = new Button();
+            lblRaton = new Label();
+            label1 = new Label();
+            txtNumeros = new TextBox();
+            lblSoloLetras = new Label();
+            txtLetras = new TextBox();
+            SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            lblNumero.AutoSize = true;
+            lblNumero.Location = new Point(80, 40);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(51, 15);
+            lblNumero.TabIndex = 0;
+            lblNumero.Text = "Numero";
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(80, 63);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(100, 23);
+            txtNumero.TabIndex = 1;
+            // 
+            // lblResultado
+            // 
+            lblResultado.AutoSize = true;
+            lblResultado.Location = new Point(80, 113);
+            lblResultado.Name = "lblResultado";
+            lblResultado.Size = new Size(0, 15);
+            lblResultado.TabIndex = 2;
+            // 
+            // btnDobleValor
+            // 
+            btnDobleValor.Location = new Point(244, 40);
+            btnDobleValor.Name = "btnDobleValor";
+            btnDobleValor.Size = new Size(91, 67);
+            btnDobleValor.TabIndex = 3;
+            btnDobleValor.Text = "DobleValor";
+            btnDobleValor.UseVisualStyleBackColor = true;
+            btnDobleValor.Click += btnDobleValor_Click;
+            // 
+            // btnDobleReferencia
+            // 
+            btnDobleReferencia.Location = new Point(246, 113);
+            btnDobleReferencia.Name = "btnDobleReferencia";
+            btnDobleReferencia.Size = new Size(89, 68);
+            btnDobleReferencia.TabIndex = 4;
+            btnDobleReferencia.Text = "Doble Referencia";
+            btnDobleReferencia.UseVisualStyleBackColor = true;
+            btnDobleReferencia.Click += btnDobleReferencia_Click;
+            // 
+            // btnObjetoReferencia
+            // 
+            btnObjetoReferencia.Location = new Point(248, 187);
+            btnObjetoReferencia.Name = "btnObjetoReferencia";
+            btnObjetoReferencia.Size = new Size(87, 72);
+            btnObjetoReferencia.TabIndex = 5;
+            btnObjetoReferencia.Text = "Objeto referencia";
+            btnObjetoReferencia.UseVisualStyleBackColor = true;
+            btnObjetoReferencia.Click += btnObjetoReferencia_Click;
+            // 
+            // lblRaton
+            // 
+            lblRaton.BackColor = Color.FromArgb(128, 255, 128);
+            lblRaton.Location = new Point(402, 140);
+            lblRaton.Name = "lblRaton";
+            lblRaton.Size = new Size(202, 113);
+            lblRaton.TabIndex = 6;
+            lblRaton.Text = "lblRaton";
+            lblRaton.TextAlign = ContentAlignment.MiddleCenter;
+            lblRaton.MouseMove += lblRaton_MouseMove;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(401, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Solo numeros";
+            // 
+            // txtNumeros
+            // 
+            txtNumeros.Location = new Point(510, 46);
+            txtNumeros.Name = "txtNumeros";
+            txtNumeros.Size = new Size(100, 23);
+            txtNumeros.TabIndex = 8;
+            txtNumeros.KeyPress += txtNumeros_KeyPress;
+            // 
+            // lblSoloLetras
+            // 
+            lblSoloLetras.AutoSize = true;
+            lblSoloLetras.Location = new Point(404, 94);
+            lblSoloLetras.Name = "lblSoloLetras";
+            lblSoloLetras.Size = new Size(61, 15);
+            lblSoloLetras.TabIndex = 9;
+            lblSoloLetras.Text = "Solo letras";
+            // 
+            // txtLetras
+            // 
+            txtLetras.Location = new Point(511, 93);
+            txtLetras.Name = "txtLetras";
+            txtLetras.Size = new Size(100, 23);
+            txtLetras.TabIndex = 10;
+            txtLetras.KeyPress += txtLetras_KeyPress;
+            // 
+            // Form14Metodos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtLetras);
+            Controls.Add(lblSoloLetras);
+            Controls.Add(txtNumeros);
+            Controls.Add(label1);
+            Controls.Add(lblRaton);
+            Controls.Add(btnObjetoReferencia);
+            Controls.Add(btnDobleReferencia);
+            Controls.Add(btnDobleValor);
+            Controls.Add(lblResultado);
+            Controls.Add(txtNumero);
+            Controls.Add(lblNumero);
+            Name = "Form14Metodos";
+            Text = "Form14Metodos";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNumero;
+        private TextBox txtNumero;
+        private Label lblResultado;
+        private Button btnDobleValor;
+        private Button btnDobleReferencia;
+        private Button btnObjetoReferencia;
+        private Label lblRaton;
+        private Label label1;
+        private TextBox txtNumeros;
+        private Label lblSoloLetras;
+        private TextBox txtLetras;
+    }
+}
---
+++ NetCoreFundamentos/Form14Metodos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form14Metodos : Form
+    {
+        public Form14Metodos()
+        {
+            InitializeComponent();
+        }
+
+        void GetDobleValor(int numero)
+        {
+            numero = numero * 2;
+        }
+
+        int GetDoble(int num)
+        {
+            return num * 2;
+        }
+
+        private void btnDobleValor_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(this.txtNumero.Text);
+            this.GetDobleValor(numero);
+            int resultado = this.GetDoble(numero);
+            this.lblResultado.Text = resultado.ToString();
+        }
+
+        void CambiarColor(Button boton)
+        {
+            boton.BackColor = Color.LightBlue;
+        }
+
+        private void btnObjetoReferencia_Click(object sender, EventArgs e)
+        {
+            this.CambiarColor(this.btnDobleValor);
+            this.CambiarColor(this.btnDobleReferencia);
+        }
+
+        void GetDobleValorReferencia(ref int numero)
+        {
+            numero = numero * 2;
+        }
+
+        private void btnDobleReferencia_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(this.txtNumero.Text);
+            this.GetDobleValorReferencia(ref numero);
+            this.lblResultado.Text = numero.ToString();
+        }
+
+        private void lblRaton_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.lblRaton.Text = "X: " + e.X + " Y: " + e.Y;
+        }
+
+        private void txtNumeros_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char teclaBorrar = (char) Keys.Back;
+            if (char.IsDigit(e.KeyChar) == false && e.KeyChar!= teclaBorrar)
+            {
+                //Indica si nos hacemos cargo del evento de key
+                e.Handled = true;
+
+            }
+            
+        }
+
+        private void txtLetras_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(char.IsLetter(e.KeyChar) == false)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
