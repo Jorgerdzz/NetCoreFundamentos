6088decc586a6503857131dd297670ce0b5fa729
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2026-01-09 09:59:30 +0100

    lectura y escritura de ficheros

---
+++ NetCoreFundamentos/Form21Files.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace NetCoreFundamentos
+{
+    partial class Form21Files
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtContenido = new TextBox();
+            lblNombre = new Label();
+            txtNombre = new TextBox();
+            btnNuevoNombre = new Button();
+            btnLeerFile = new Button();
+            btnWriteFile = new Button();
+            label3 = new Label();
+            lstNombres = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(54, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Contenido File";
+            // 
+            // txtContenido
+            // 
+            txtContenido.Location = new Point(57, 73);
+            txtContenido.Multiline = true;
+            txtContenido.Name = "txtContenido";
+            txtContenido.Size = new Size(255, 205);
+            txtContenido.TabIndex = 1;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(366, 41);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(51, 15);
+            lblNombre.TabIndex = 2;
+            lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(366, 73);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(120, 23);
+            txtNombre.TabIndex = 3;
+            // 
+            // btnNuevoNombre
+            // 
+            btnNuevoNombre.Location = new Point(366, 118);
+            btnNuevoNombre.Name = "btnNuevoNombre";
+            btnNuevoNombre.Size = new Size(120, 23);
+            btnNuevoNombre.TabIndex = 4;
+            btnNuevoNombre.Text = "Nuevo nombre";
+            btnNuevoNombre.UseVisualStyleBackColor = true;
+            btnNuevoNombre.Click += btnNuevoNombre_Click;
+            // 
+            // btnLeerFile
+            // 
+            btnLeerFile.Location = new Point(366, 164);
+            btnLeerFile.Name = "btnLeerFile";
+            btnLeerFile.Size = new Size(120, 23);
+            btnLeerFile.TabIndex = 5;
+            btnLeerFile.Text = "Leer file";
+            btnLeerFile.UseVisualStyleBackColor = true;
+            btnLeerFile.Click += this.btnLeerFile_Click;
+            // 
+            // btnWriteFile
+            // 
+            btnWriteFile.Location = new Point(366, 210);
+            btnWriteFile.Name = "btnWriteFile";
+            btnWriteFile.Size = new Size(120, 23);
+            btnWriteFile.TabIndex = 6;
+            btnWriteFile.Text = "Wirte file";
+            btnWriteFile.UseVisualStyleBackColor = true;
+            btnWriteFile.Click += btnWriteFile_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(550, 34);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Nombres";
+            // 
+            // lstNombres
+            // 
+            lstNombres.FormattingEnabled = true;
+            lstNombres.Location = new Point(550, 73);
+            lstNombres.Name = "lstNombres";
+            lstNombres.Size = new Size(189, 199);
+            lstNombres.TabIndex = 8;
+            // 
+            // Form21Files
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstNombres);
+            Controls.Add(label3);
+            Controls.Add(btnWriteFile);
+            Controls.Add(btnLeerFile);
+            Controls.Add(btnNuevoNombre);
+            Controls.Add(txtNombre);
+            Controls.Add(lblNombre);
+            Controls.Add(txtContenido);
+            Controls.Add(label1);
+            Name = "Form21Files";
+            Text = "Form21Files";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtContenido;
+        private Label lblNombre;
+        private TextBox txtNombre;
+        private Button btnNuevoNombre;
+        private Button btnLeerFile;
+        private Button btnWriteFile;
+        private Label label3;
+        private ListBox lstNombres;
+    }
+}
---
+++ NetCoreFundamentos/Form21Files.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace NetCoreFundamentos
+{
+    public partial class Form21Files : Form
+    {
+
+        public string Path { get; set; }
+
+        public Form21Files()
+        {
+            InitializeComponent();
+            //TENEMOS DOS FORMAS PARA ESCRIBIR LAS RUTAS
+            //1) C:\carperta\1.txt
+            //this.Path = "C:\\carpeta\\1.txt"; CON DOBLE BARRA
+            //2) Utilizar @ antes del String y fuera del String
+            //this.Path = @"C:\\carpeta\\1.txt";
+
+            this.Path = "file1.txt";
+
+        }
+
+        private void btnNuevoNombre_Click(object sender, EventArgs e)
+        {
+            this.lstNombres.Items.Add(this.txtNombre.Text);
+            this.txtNombre.SelectAll();
+            this.txtNombre.Focus();
+        }
+
+        private async void btnLeerFile_Click(object sender, EventArgs e)
+        {
+            FileInfo file = new FileInfo(this.Path);
+            using (TextReader reader = file.OpenText())
+            {
+                string content = await reader.ReadToEndAsync();
+                reader.Close();
+                this.txtContenido.Text = content;
+            }
+        }
+
+        private async void btnWriteFile_Click(object sender, EventArgs e)
+        {
+            //TENEMOS LA CLASE FileInfo QUE NOS DEVUELVE UN FILE Y PODEMOS GENERAR UN WRITER/READER
+            FileInfo file = new FileInfo(this.Path);
+
+            //CREAMOS FICHERO
+            using (TextWriter writer = file.CreateText())
+            {
+                string content = this.GetNombresListBox();
+
+                //ESCRIBIMOS EN EL FICHERO
+                await writer.WriteAsync(content);
+
+                //DESPUES DE ESCRIBIR EN CUALQUIER FICHERO, DEBEMOS LIBERAR LA MEMORIA PARA HACERLO FIJO
+                await writer.FlushAsync();
+
+                //CERRAMOS EL FICHERO
+                writer.Close();
+                MessageBox.Show("Fichero almacenado");
+            }
+        }
+
+        public string GetNombresListBox()
+        {
+            string data = "";
+            foreach(string name in this.lstNombres.Items)
+            {
+                data += name + ", ";
+            }
+            data = data.Trim(',');
+            return data;
+        }
+    }
+}
